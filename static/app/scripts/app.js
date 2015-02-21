855c3ddb0baf4136ac3135f889fb8ddd0ddfd88d
Andy Y <andy.yap.cal@gmail.com>
2015-02-20 16:39:34 -0800

    First commit with pip freeze and yo angular done

---
+++ static/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name declutterApp
+ * @description
+ * # declutterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('declutterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ static/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name declutterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the declutterApp
+ */
+angular.module('declutterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
