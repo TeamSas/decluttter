4f7b41201ec1af6e4c49c78802571b6b8ef110dc
skumo <skathimanda@gmail.com>
2015-02-26 15:05:07 -0800

    commit 5

--- static/app/scripts/controllers/friendscrap.js
+++ static/app/scripts/controllers/friendscrap.js
@@ -6,15 +6,16 @@
 
 angular.module('declutterApp')
   .controller('FriendscrapCtrl', function ($scope, $http) {
-    var friends = $http.get('/api/appuser/list/follower/');
+        var arrayOfFollowers = [];
+        var friends = $http.get('/api/appuser/list/follower/');
         friends.success(function(data){
             for (var i=0;  i<data.length; i++) {
-                console.log("api/list/follower success " + data[i].follower);
-            }
+                 arrayOfFollowers.push(data[i]["follower"]);
+                 }
+            console.log(arrayOfFollowers);
             $scope.friends=data;
     }).
         error(function(data){
-//        $scope.error = ["Error with friends"];
         console.log("error with friends " + data);
     });
 
@@ -22,14 +23,25 @@ angular.module('declutterApp')
 
 
     //   Get all the data from Item List API
+    var arrayOfObjects = [];
     var poster_items = $http.get('/api/items/');
         poster_items.success(function(data){
             for (var i=0;  i<data.length; i++) {
-                console.log("Item List API" + data[i]["poster"]["username"]);
-                }
+                var userName =  data[i]["poster"]["username"];
+                var userId = data[i]["id"];
+                var item_Name = data[i]["item_name"];
+                // Initialize with empty object and then add user & item details
+                var obj = {};
+                obj["id"] = userId;
+                obj["username"] = userName;
+                obj["item"] = item_Name;
+                arrayOfObjects.push(obj);
+            }
+            console.log(arrayOfObjects);
                 $scope.poster_items = data;
         }).
         error(function(data){
                 console.log("error with poster_items " + data);
         });
   });
+
